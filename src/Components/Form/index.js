1283137afe16eef17fb8b9524c6ffc15bc407c1e
Igor <igormgg@hotmail.com>
2021-09-13 20:39:04 -0300

    feat: initial form stylization

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -2,16 +2,20 @@ import "./styles.css";
 
 const Form = () => {
   return (
-    <div className="form_box">
+    <div>
       <h1>Este site não é uma fraude</h1>
-      <form>
+      <form className="form_box">
         <input placeholder="Nome Completo" />
         <input placeholder="CPF" />
         <input placeholder="Data de Nascimento" />
         <input placeholder="Número do Cartão de Crédito" />
         <input placeholder="Nome do Titular (Igual ao Cartão)" />
-        <input placeholder="Validade" />
-        <input placeholder="Código de segurança" />
+        <div className="form_box__expiry_code">
+          <p>Data de Validade</p>
+          <p>Código de Segurança</p>
+          <input placeholder="Validade" type="date" />
+          <input placeholder="CVV" />
+        </div>
         <button>Enviar</button>
       </form>
     </div>
