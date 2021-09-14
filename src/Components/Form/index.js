4b22da838eed9963ae0f72965b3713ac75f3da14
Igor <igormgg@hotmail.com>
2021-09-14 00:07:06 -0300

    feat: added some form_box stylization

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -2,12 +2,15 @@ import { useForm } from "react-hook-form";
 import "./styles.css";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import { useState } from "react";
 
 const Form = () => {
+  const [showCard, setShowCard] = useState(false);
+
   const formSchema = yup.object().shape({
     email: yup
       .string()
-      .required("Digite o email")
+      .required("*Digite o email")
       .email("Email inválido")
       .matches(
         "^([-a-zA-Z0-9_-]*@(gmail|yahoo|ymail|rocketmail|bol|hotmail|live|msn|ig|globomail|oi|pop|inteligweb|r7|folha|zipmail).(com|info|gov|net|org|tv)(.[-a-z]{2})?)*$",
@@ -15,27 +18,27 @@ const Form = () => {
       ),
     password: yup
       .string()
-      .required("Digite a senha")
+      .required("*Digite a senha")
       .matches(
         "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})",
         "A senha deve conter pelo menos 8 caracteres, sendo um minúsculo, um maiúsculo, um número, e um caracter especial (!@#$%^&*)"
       ),
     cpf: yup
       .string()
-      .required("Digite o CPF")
+      .required("*Digite o CPF")
       .matches("^([0-9]{3}.?[0-9]{3}.?[0-9]{3}-?[0-9]{2})$", "CPF inválido"),
     cardNumber: yup
       .string()
-      .required("Digite o número do cartão")
+      .required("*Digite o número do cartão")
       .matches(
         "^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35d{3})d{11})$",
         "Cartão inválido"
       ),
-    ownerName: yup.string().required("Digite o nome do titular"),
-    validThrough: yup.string().required("Selecione a validade do cartão"),
+    ownerName: yup.string().required("*Digite o nome do titular"),
+    validThrough: yup.string().required("*Selecione a validade do cartão"),
     cvv: yup
       .string()
-      .required("Digite o código de segurança")
+      .required("*Digite o código de segurança")
       .matches("^([0-9]{3})$", "CVV inválido"),
   });
 
@@ -47,33 +50,59 @@ const Form = () => {
     resolver: yupResolver(formSchema),
   });
 
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    return console.log(data);
+  };
 
-  console.log(errors);
+  // const onSubmit = (data) => {
+  //     return (
+  //         <div>{data.email)</div>
+  //     )
+  // };
 
   return (
     <div>
       <h1>Este site não é uma fraude</h1>
+      <h3>(É verdade esse bilete)</h3>
       <form className="form_box" onSubmit={handleSubmit(onSubmit)}>
-        <input placeholder="Email" {...register("email")} type="email" />
-        <p>{errors.email?.message}</p>
-        <input placeholder="Senha" {...register("password")} type="password" />
-        <p>{errors.password?.message}</p>
-        <input placeholder="CPF" {...register("cpf")} type="number" />
-        <p>{errors.cpf?.message}</p>
-        <input
-          placeholder="Número do Cartão de Crédito"
-          {...register("cardNumber")}
-          type="text"
-        />
-        <p>{errors.cardNumber?.message}</p>
-        <input
-          placeholder="Nome do Titular (Igual ao Cartão)"
-          {...register("ownerName")}
-          type="text"
-          maxLength="18"
-        />
-        <p>{errors.ownerName?.message}</p>
+        <div>
+          <h5>Email</h5>
+          <input placeholder="Email" {...register("email")} type="email" />
+          <p>{errors.email?.message}</p>
+        </div>
+        <div>
+          <h5>Senha</h5>
+          <input
+            placeholder="Senha"
+            {...register("password")}
+            type="password"
+          />
+          <p>{errors.password?.message}</p>
+        </div>
+        <div>
+          <h5>CPF</h5>
+          <input placeholder="CPF" {...register("cpf")} type="number" />
+          <p>{errors.cpf?.message}</p>
+        </div>
+        <div>
+          <h5>Número do cartão de Crédito</h5>
+          <input
+            placeholder="Número do Cartão de Crédito"
+            {...register("cardNumber")}
+            type="text"
+          />
+          <p>{errors.cardNumber?.message}</p>
+        </div>
+        <div>
+          <h5>Nome do titular do Cartão</h5>
+          <input
+            placeholder="Nome do Titular (Igual ao Cartão)"
+            {...register("ownerName")}
+            type="text"
+            maxLength="18"
+          />
+          <p>{errors.ownerName?.message}</p>
+        </div>
         <div className="form_box__expiry_code">
           <h5>Data de Validade</h5>
           <h5>Código de Segurança</h5>
@@ -91,8 +120,9 @@ const Form = () => {
           <p>{errors.validThrough?.message}</p>
           <p>{errors.cvv?.message}</p>
         </div>
-        <button>Enviar</button>
+        <button onClick={() => setShowCard(true)}>Clonar</button>
       </form>
+      {showCard && <div>ESTE É O MEU CARTÃO</div>}
     </div>
   );
 };
